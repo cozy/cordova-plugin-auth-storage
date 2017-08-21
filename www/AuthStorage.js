42ee52e97437d02a227b9ede821432ffdf61c718
Olivier <maestun@free.fr>
2017-08-21 13:49:48 +0200

    added auth removal

--- www/AuthStorage.js
+++ www/AuthStorage.js
@@ -4,6 +4,11 @@ var authStorage = {
     storeData: function(serverURL, authToken, onSuccess, onError) {
         console.log('AuthStorage plugin: calling storeData w/' + serverURL + " / " + authToken);
         cordova.exec(onSuccess, onError, 'AuthStorage', 'storeData', [serverURL, authToken]);
+    },
+
+    removeData: function(onSuccess, onError) {
+        console.log('AuthStorage plugin: calling removeData');
+        cordova.exec(onSuccess, onError, 'AuthStorage', 'removeData', []);
     }
 };
 
